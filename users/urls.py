a21c2d34a5ba6398b978937b6ebb8fdefd32080e
Nadezhda Lyovushkina <lyovushkina.nv@gmail.com>
2024-03-28 22:59:05 +0300

    paths: User(CRUD) and tokens

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import UserUpdateAPIView, PaymentListAPIView
+from users.views import UserCreateAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView, \
+    PaymentListAPIView, UserListAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('profile/update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('list/', UserListAPIView.as_view(), name='user_list'),
+    path('user/create/', UserCreateAPIView.as_view(), name='user_create'),
+    path('user/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_detail'),
+    path('user/<int:pk>/update/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('user/<int:pk>/delete/', UserDestroyAPIView.as_view(), name='user_delete'),
 
     path('payment/', PaymentListAPIView.as_view(), name='payment_list'),
+
+    # tokens
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
